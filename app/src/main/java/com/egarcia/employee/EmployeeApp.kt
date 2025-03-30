33376072e9e2361ae2015e9c3e1e5b0144013546
Erick Garcia <erick@egarcia.dev>
2025-03-30 15:25:22 -0700

    REQ008: Additional cleanup before publishing

--- app/src/main/java/com/egarcia/employee/EmployeeApp.kt
+++ app/src/main/java/com/egarcia/employee/EmployeeApp.kt
@@ -4,5 +4,5 @@ import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class BlockEmployeeApp : Application() {
+class EmployeeApp : Application() {
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -19,6 +19,6 @@ dependencyResolutionManagement {
     }
 }
 
-rootProject.name = "BlockEmployeeDirectory"
+rootProject.name = "eEmployeeDirectory"
 include(":app")
  
